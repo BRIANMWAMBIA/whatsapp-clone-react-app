ed15f36e304a7919fc4571c406b864feefe8fb0b
brian njoka <nels@kali>
2023-02-08 12:57:03 +0300

    added realtime-chat

--- src/App.js
+++ src/App.js
@@ -1,13 +1,57 @@
 
+import React,{ useEffect, useState } from 'react';
+
 import './App.css';
 import Chat from './Chat';
 import Sidebar from'./Sidebar';
+import axios from './axios';
+
+import Pusher from 'pusher-js';
 function App() {
+  //fetch all the messages when the application loads up --axios
+  const [messages, setMessages] = useState([]);
+  useEffect(() =>{
+    axios.get('/api/messages/sync')
+    .then((response)=>{
+     
+      setMessages(response.data);
+    })
+
+  })
+  //loop through the array and display the messages on the chat
+  //append the new message to existing array and updatethe chat
+  //subscribe to the messages channel on pusher and bind to inserted event to get the event and the change when it occurs in the database
+  //
+  useEffect(() => {
+    const pusher = new Pusher('e85842f3b311ed6b933a', {
+      cluster: 'mt1'
+    });
+    const channel = pusher.subscribe('messages');
+    channel.bind("inserted", (newMessage) => {
+      alert(JSON.stringify(newMessage));
+      setMessages([...messages, newMessage])
+      console.log(newMessage);
+      //cleaner function to unsubscribe, ensure wehave only one subscriber
+      return () => {
+        channel.unbind_all();
+        channel.unsubscribe()
+      }
+   
+    });
+
+  }, [messages]);
+
+  //runs once when the app loads 
+  //console.log(messages);
+  //subscribe to the messages channel
+ 
+
+
   return (
     <div className="app">
       <div className='app_body'>
       <Sidebar />
-     <Chat />
+     <Chat messages={messages}/>
       </div>
 
     </div>
--- src/Chat.js
+++ src/Chat.js
@@ -1,12 +1,23 @@
 import { Avatar, IconButton } from '@material-ui/core';
 import { AttachFile, InsertEmoticon, Mic, MoreVert, Search } from '@material-ui/icons';
-import React from 'react'
+import React, { useState } from 'react'
+import axios from "./axios";
 import "./Chat.css"
-function Chat() {
+function Chat({messages}) {
+    const [input, setInput]=useState("")
+    const sendMessage = async (e)=>{
+        e.preventDefault();
 
-    function getAllMessages(){
-        
+        await axios.post("/api/messages/new",
+        {
+            message:input,
+            "name":"brian ",
+            "timestamp":"test time",
+            "received": false
+        })
+        setInput("");
     }
+
     return <div className='chat'>
        <div className='chat__header'>
         <Avatar />
@@ -27,78 +38,22 @@ function Chat() {
         </div>
         </div>
         <div className='chat__body'>
-            <p className='chat__message'>
-                <span className='chat__name'>Brian</span>
-                This is crazy! It's working..
-                <span className='chat__timestamp'>
-                    {new Date().toUTCString()}
-                </span>   
-            </p>
-            <p className='chat__message chat__receiver'>
-                <span className='chat__name'>Brian</span>
-                This is crazy! It's working..
-                <span className='chat__timestamp'>
-                    {new Date().toUTCString()}
-                </span>   
-            </p>
-            <p className='chat__message'>
-                <span className='chat__name'>Brian</span>
-                This is crazy! It's working..
-                <span className='chat__timestamp'>
-                    {new Date().toUTCString()}
-                </span>   
-            </p>
-            <p className='chat__message chat__receiver'>
-                <span className='chat__name'>Brian</span>
-                This is crazy! It's working..
-                <span className='chat__timestamp'>
-                    {new Date().toUTCString()}
-                </span>   
-            </p>
-            <p className='chat__message chat_receiver'>
-                <span className='chat__name'>Brian</span>
-                This is crazy! It's working..
-                <span className='chat__timestamp'>
-                    {new Date().toUTCString()}
-                </span>   
-            </p>
-            <p className='chat__message'>
-                <span className='chat__name'>Brian</span>
-                This is crazy! It's working..
-                <span className='chat__timestamp'>
-                    {new Date().toUTCString()}
-                </span>   
-            </p>
-            <p className='chat__message'>
-                <span className='chat__name'>Brian</span>
-                This is crazy! It's working..
-                <span className='chat__timestamp'>
-                    {new Date().toUTCString()}
-                </span>   
-            </p>
-            <p className='chat__message chat__receiver'>
-                <span className='chat__name'>Brian</span>
-                This is crazy! It's working..
-                <span className='chat__timestamp'>
-                    {new Date().toUTCString()}
-                </span>   
-            </p>
-            <p className='chat__message'>
-                <span className='chat__name'>Brian</span>
-                This is crazy! It's working..
-                <span className='chat__timestamp'>
-                    {new Date().toUTCString()}
-                </span>   
-            </p>
-          
-          
+            {messages.map((message)=>(
+                       <p className={`chat__message ${message.received && "chat__receiver"}`} >
+                       <span className='chat__name'>{message.name}</span>
+                       {message.message}
+                       <span className='chat__timestamp'>
+                           {message.timestamp}
+                       </span>   
+                   </p>
+            ))}
             
         </div>
        <div className='chat__footer'>
         <InsertEmoticon />
         <form>
-            <input type="text" placeholder='Type a message' />
-            <button type='submit'>Send a message</button>
+            <input value={input} onChange={(e) => setInput(e.target.value)} type="text" placeholder='Type a message' />
+            <button onClick={sendMessage} type='submit'>Send a message</button>
         </form>
         <Mic/>
        </div>
